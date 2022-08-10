49e876c5fcbce675efef926867220988205c30f8
Julia <julia.panfilova@mail.ru>
2022-08-10 22:19:12 +0300

    new tests added

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -17,4 +17,40 @@ public class StatisticsServiceTest {
 
         assertEquals(expected, actual);
     }
+
+    @Test
+    void findMaxBetweenEnds() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 100, 12};
+        long expected = 100;
+
+        long actual = service.findMax(incomesInBillions);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void findMaxBetweenNegatives() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {-12, -102};
+        long expected = -12;
+
+        long actual = service.findMax(incomesInBillions);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void findMaxFromEquals() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {125, 125};
+        long expected = 125;
+
+        long actual = service.findMax(incomesInBillions);
+
+        assertEquals(expected, actual);
+    }
 }
